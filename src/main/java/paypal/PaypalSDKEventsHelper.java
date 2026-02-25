5683ba98e9fc237cea678accc9e22b3614a37cd7
Giulio Speri <giulio.speri@mpstyle.it>
2026-02-25 17:46:25 +0100

    [F2E | MIG | plugin PAYPAL | fix recupero dati json da request attributes]

--- src/main/java/paypal/PaypalSDKEventsHelper.java
+++ src/main/java/paypal/PaypalSDKEventsHelper.java
@@ -3,6 +3,8 @@ package paypal;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.apache.ofbiz.base.util.Debug;
+import org.apache.ofbiz.base.util.UtilHttp;
+import org.apache.ofbiz.base.util.UtilMisc;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
@@ -25,6 +27,11 @@ public class PaypalSDKEventsHelper {
 
         String orderId = null;
 
+        Map<String, Object> requestAttributeMap = UtilHttp.getAttributeMap(request);
+        Debug.logWarning("(getOrderIdFromRequestBody) request attribute map =>\n" + UtilMisc.printMap(requestAttributeMap), module);
+
+/* Not working in OFBiz-16+, since JSON data in requests are
+stripped by ContextFilter and put directly into the request as attributes.
         try {
             BufferedReader reader = request.getReader();
             StringBuilder buffer = new StringBuilder();
@@ -46,6 +53,11 @@ public class PaypalSDKEventsHelper {
             request.setAttribute("_ERROR_MESSAGE_", msg);
             return null;
         }
+
+ */
+
+        orderId = request.getAttribute("orderId") != null ? (String) request.getAttribute("orderId") : null;
+
         return orderId;
     }
     /**
@@ -59,7 +71,10 @@ public class PaypalSDKEventsHelper {
         String paypalOrderId = null;
         String orderId = null;
 
+        Map<String, Object> requestAttributeMap = UtilHttp.getAttributeMap(request);
+        Debug.logWarning("(getOrderIdsFromRequestBody) request attribute map =>\n" + UtilMisc.printMap(requestAttributeMap), module);
 
+/*
         try {
             BufferedReader reader = request.getReader();
             StringBuilder buffer = new StringBuilder();
@@ -84,6 +99,15 @@ public class PaypalSDKEventsHelper {
             request.setAttribute("_ERROR_MESSAGE_", msg);
             return null;
         }
+
+ */
+        paypalOrderIdMap = new HashMap<>();
+        paypalOrderId = (request.getAttribute("paypalOrderId") != null) ? (String) request.getAttribute("paypalOrderId") : null;
+        orderId = (request.getAttribute("orderId") != null) ? (String) request.getAttribute("orderId") : null;
+        paypalOrderIdMap.put("paypalOrderId", paypalOrderId);
+        paypalOrderIdMap.put("orderId", orderId);
+
+
         return paypalOrderIdMap;
     }
 
@@ -96,6 +120,10 @@ public class PaypalSDKEventsHelper {
 
         Map<String, String> hostedCardFieldsMap = new HashMap<>();
 
+        Map<String, Object> requestAttributeMap = UtilHttp.getAttributeMap(request);
+        Debug.logWarning("(getHostedFieldsOrderParametersFromRequestBody) request attribute map =>\n" + UtilMisc.printMap(requestAttributeMap), module);
+
+        /*
         try {
             BufferedReader reader = request.getReader();
             StringBuilder buffer = new StringBuilder();
@@ -135,6 +163,29 @@ public class PaypalSDKEventsHelper {
             request.setAttribute("_ERROR_MESSAGE_", msg);
             return null;
         }
+
+         */
+
+        String orderId = (String) request.getAttribute("orderId");
+        String paymentMethod = (String) request.getAttribute("paymentMethod");
+        String cardHolderName = (request.getAttribute("cardHolderName") != null) ? (String) request.getAttribute("cardHolderName") : "";
+        String cardStreetAddress = (request.getAttribute("streetAddress") != null) ? (String) request.getAttribute("streetAddress") : "";
+        String cardExtendedAddress = (request.getAttribute("extendedAddress") != null) ? (String) request.getAttribute("extendedAddress") : "";
+        String cardAddressState = (request.getAttribute("state") != null) ? (String) request.getAttribute("state") : "";
+        String cardAddressCity = (request.getAttribute("city") != null) ? (String) request.getAttribute("city") : "";
+        String cardAddressPostalCode = (request.getAttribute("postalCode") != null) ? (String) request.getAttribute("postalCode") : "";
+        String cardAddressCountryCodeAlpha2 = (request.getAttribute("countryCodeAlpha2") != null) ? (String) request.getAttribute("countryCodeAlpha2") : "";
+
+        hostedCardFieldsMap.put("orderId", orderId);
+        hostedCardFieldsMap.put("paymentMethod", paymentMethod);
+        hostedCardFieldsMap.put("cardHolderName", cardHolderName);
+        hostedCardFieldsMap.put("cardStreetAddress", cardStreetAddress);
+        hostedCardFieldsMap.put("cardExtendedAddress", cardExtendedAddress);
+        hostedCardFieldsMap.put("cardAddressState", cardAddressState);
+        hostedCardFieldsMap.put("cardAddressCity", cardAddressCity);
+        hostedCardFieldsMap.put("cardAddressPostalCode", cardAddressPostalCode);
+        hostedCardFieldsMap.put("cardAddressCountryCodeAlpha2", cardAddressCountryCodeAlpha2);
+
 
         return hostedCardFieldsMap;
     }
